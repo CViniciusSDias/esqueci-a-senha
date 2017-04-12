c9bcb0fcac093106ecbe37b4596b96534a46eaa7
Vinicius Dias <carlosv775@gmail.com>
2017-04-11 23:17:52 -0300

    Removed unused code.
    Added rootPage verification.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
-
 import { SlidesPage } from '../pages/slides/slides';
-
+import { LoginPage } from '../pages/login/login';
 
 @Component({
     templateUrl: 'app.html'
 })
 export class MyApp {
-    rootPage = SlidesPage;
+    public rootPage;
 
     constructor(platform: Platform) {
         platform.ready().then(() => {
-            // Okay, so the platform is ready and our plugins are available.
-            // Here you can do any higher level native things you might need.
-            StatusBar.styleDefault();
-            Splashscreen.hide();
+            if (localStorage.getItem('firstView') === '0') {
+                this.rootPage = LoginPage;
+                return;
+            }
+
+            this.rootPage = SlidesPage;
         });
     }
 }
