11f343639c3eee1a99361d5fe3a99d00c4749df3
Vinicius Dias <carlosv775@gmail.com>
2017-04-11 23:29:07 -0300

    Removed unused import.

--- src/pages/slides/slides.ts
+++ src/pages/slides/slides.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { LoginPage } from '../login/login';
 import { PerguntaPage } from './../pergunta/pergunta';
 
 @Component({
