3bc35382ebb2bc067f160ed010ec70fca092464f
Vinicius Dias <carlosv775@gmail.com>
2017-04-14 15:28:38 -0300

    Getters e setters em uma só linha

--- src/models/senha.ts
+++ src/models/senha.ts
@@ -5,29 +5,14 @@ export class Senha {
     private _senha: string;
     private _ondeUsar: string;
 
-    public get id(): number {
-        return this._id;
-    }
+    public get id(): number { return this._id; }
+    public set id(id: number) { this._id = id; }
 
-    public set id(id: number) {
-        this._id = id;
-    }
+    public get senha(): string { return this._senha; }
+    public set senha(senha: string) { this._senha = senha }
 
-    public get senha(): string {
-        return this._senha;
-    }
-
-    public set senha(senha: string) {
-        this._senha = senha
-    }
-
-    public get ondeUsar(): string {
-        return this._ondeUsar;
-    }
-
-    public set ondeUsar(ondeUsar: string) {
-        this._ondeUsar = ondeUsar
-    }
+    public get ondeUsar(): string { return this._ondeUsar; }
+    public set ondeUsar(ondeUsar: string) { this._ondeUsar = ondeUsar }
 
     public estaValida(): boolean {
         return !StringHelper.empty(this._senha) && !StringHelper.empty(this._ondeUsar);
