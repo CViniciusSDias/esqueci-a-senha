2f529ea70de4d30b1d5dc88bfbe4182c6dbceced
Vinicius Dias <carlosv775@gmail.com>
2019-03-19 18:32:46 -0300

    Preload dos demais módulos implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes/*, { preloadingStrategy: PreloadAllModules }*/) // TODO: checar o preload
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
