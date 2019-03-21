070f8c7bba5b334a6e9cc8e243429207b9f9e424
Vinicius Dias <carlosv775@gmail.com>
2019-03-21 20:42:07 -0300

    RouterTestingModule adicionado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('AppComponent', () => {
 
@@ -25,6 +26,9 @@ describe('AppComponent', () => {
         { provide: SplashScreen, useValue: splashScreenSpy },
         { provide: Platform, useValue: platformSpy },
       ],
+      imports: [
+        RouterTestingModule
+      ]
     }).compileComponents();
   }));
 
