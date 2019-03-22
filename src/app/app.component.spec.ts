523abb3e96d5d0d492cad44e15cd60b27d3cdc11
Vinicius Dias <carlosv775@gmail.com>
2019-03-21 21:32:20 -0300

    Imports atualizados

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
-import {RouterTestingModule} from '@angular/router/testing';
 
 describe('AppComponent', () => {
 
